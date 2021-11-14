c9ee9893de5f2377e44b44204d0c091d8e110fb2
HarrievG <Harry@vrknight.eu>
2021-11-14 12:05:02 +0100

    fixed main loop + drawing

--- src/main.cpp
+++ src/main.cpp
@@ -36,64 +36,65 @@ void main_loop( void *data ) {
             break;
         }
     }
-//
-//    ImGui_Implbgfx_NewFrame( );
-//    ImGui_ImplSDL2_NewFrame( context->window );
-//
-//    ImGui::NewFrame( );
-//    ImGui::ShowDemoWindow( ); // your drawing here
-//    ImGui::Render( );
-//    ImGui_Implbgfx_RenderDrawLists( ImGui::GetDrawData( ) );
-//
-//
-//    // simple input code for orbit camera
-//    int mouse_x, mouse_y;
-//    const int buttons = SDL_GetMouseState( &mouse_x, &mouse_y );
-//    if ( ( buttons & SDL_BUTTON( SDL_BUTTON_LEFT ) ) != 0 ) {
-//        int delta_x = mouse_x - context->prev_mouse_x;
-//        int delta_y = mouse_y - context->prev_mouse_y;
-//        context->cam_yaw += float( -delta_x ) * context->rot_scale;
-//        context->cam_pitch += float( -delta_y ) * context->rot_scale;
-//    }
-//
-//    context->prev_mouse_x = mouse_x;
-//    context->prev_mouse_y = mouse_y;
-//
-//    float cam_rotation[16];
-//    bx::mtxRotateXYZ( cam_rotation, context->cam_pitch, context->cam_yaw, 0.0f );
-//
-//    float cam_translation[16];
-//    bx::mtxTranslate( cam_translation, 0.0f, 0.0f, -5.0f );
-//
-//    float cam_transform[16];
-//    bx::mtxMul( cam_transform, cam_translation, cam_rotation );
-//
-//    float view[16];
-//    bx::mtxInverse( view, cam_transform );
-//
-//    float proj[16];
-//    bx::mtxProj(
-//        proj, 60.0f, float( context->width ) / float( context->height ), 0.1f,
-//        100.0f, bgfx::getCaps( )->homogeneousDepth );
-//
-//    bgfx::setViewTransform( 0, view, proj );
-//
-//    float model[16];
-//    bx::mtxIdentity( model );
-//    bgfx::setTransform( model );
-//
-//    bgfx::setVertexBuffer( 0, context->vbh );
-//    bgfx::setIndexBuffer( context->ibh );
-//
-//    bgfx::submit( 0, context->program );
-//
-//    bgfx::frame( );
-//
-//#if BX_PLATFORM_EMSCRIPTEN
-//    if ( context->quit ) {
-//        emscripten_cancel_main_loop( );
-//    }
-//#endif
+
+    ImGui_Implbgfx_NewFrame( );
+    ImGui_ImplSDL2_NewFrame( context->window );
+
+    ImGui::NewFrame( );
+    ImGui::ShowDemoWindow( ); // your drawing here
+    ImGui::Render( );
+    ImGui_Implbgfx_RenderDrawLists( ImGui::GetDrawData( ) );
+
+
+    // simple input code for orbit camera
+    int mouse_x, mouse_y;
+    const int buttons = SDL_GetMouseState( &mouse_x, &mouse_y );
+    if ( ( buttons & SDL_BUTTON( SDL_BUTTON_LEFT ) ) != 0 ) {
+        int delta_x = mouse_x - context->prev_mouse_x;
+        int delta_y = mouse_y - context->prev_mouse_y;
+        context->cam_yaw += float( -delta_x ) * context->rot_scale;
+        context->cam_pitch += float( -delta_y ) * context->rot_scale;
+    }
+
+    context->prev_mouse_x = mouse_x;
+    context->prev_mouse_y = mouse_y;
+
+    float cam_rotation[16];
+    bx::mtxRotateXYZ( cam_rotation, context->cam_pitch, context->cam_yaw, 0.0f );
+
+    float cam_translation[16];
+    bx::mtxTranslate( cam_translation, 0.0f, 0.0f, -5.0f );
+
+    float cam_transform[16];
+    bx::mtxMul( cam_transform, cam_translation, cam_rotation );
+
+    float view[16];
+    bx::mtxInverse( view, cam_transform );
+
+    float proj[16];
+    bx::mtxProj(
+        proj, 60.0f, float( context->width ) / float( context->height ), 0.1f,
+        100.0f, bgfx::getCaps( )->homogeneousDepth );
+
+    bgfx::setViewTransform( 0, view, proj );
+
+    float model[16];
+    bx::mtxIdentity( model );
+    bgfx::setTransform( model );
+
+    //bgfx::setVertexBuffer( 0, context->vbh );
+    //bgfx::setIndexBuffer( context->ibh );
+
+    //bgfx::submit( 0, context->program );
+
+    bgfx::touch( 0 );//not really drawing so Uniforms and draw state will be applied 
+    bgfx::frame( );
+
+#if BX_PLATFORM_EMSCRIPTEN
+    if ( context->quit ) {
+        emscripten_cancel_main_loop( );
+    }
+#endif
 }
 
 int main( int argc, char **argv )
@@ -102,7 +103,6 @@ int main( int argc, char **argv )
         printf( "SDL could not initialize. SDL_Error: %s\n", SDL_GetError( ) );
         return 1;
     }
-
 
     const int width = 800;
     const int height = 600;
@@ -126,6 +126,72 @@ int main( int argc, char **argv )
     }
     bgfx::renderFrame( ); // single threaded mode
 #endif // !BX_PLATFORM_EMSCRIPTEN
-    bgfx::init();
+
+    bgfx::PlatformData pd{};
+#if BX_PLATFORM_WINDOWS
+    pd.nwh = wmi.info.win.window;
+#elif BX_PLATFORM_OSX
+    pd.nwh = wmi.info.cocoa.window;
+#elif BX_PLATFORM_LINUX
+    pd.ndt = wmi.info.x11.display;
+    pd.nwh = ( void * ) ( uintptr_t ) wmi.info.x11.window;
+#elif BX_PLATFORM_EMSCRIPTEN
+    pd.nwh = ( void * ) "#canvas";
+#endif // BX_PLATFORM_WINDOWS ? BX_PLATFORM_OSX ? BX_PLATFORM_LINUX ?
+        // BX_PLATFORM_EMSCRIPTEN
+
+    bgfx::Init bgfx_init;
+    bgfx_init.type = bgfx::RendererType::Count; // auto choose renderer
+    bgfx_init.resolution.width = width;
+    bgfx_init.resolution.height = height;
+    bgfx_init.resolution.reset = BGFX_RESET_VSYNC;
+    bgfx_init.platformData = pd;
+    bgfx::init( bgfx_init );
+
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x6495EDFF, 1.0f, 0 );
+    
+    bgfx::setViewRect( 0, 0, 0, width, height );
+
+    ImGui::CreateContext( );
+    ImGuiIO &io = ImGui::GetIO( );
+
+    ImGui_Implbgfx_Init( 255 );
+#if BX_PLATFORM_WINDOWS
+    ImGui_ImplSDL2_InitForD3D( window );
+#elif BX_PLATFORM_OSX
+    ImGui_ImplSDL2_InitForMetal( window );
+#elif BX_PLATFORM_LINUX || BX_PLATFORM_EMSCRIPTEN
+    ImGui_ImplSDL2_InitForOpenGL( window, nullptr );
+#endif // BX_PLATFORM_WINDOWS ? BX_PLATFORM_OSX ? BX_PLATFORM_LINUX ?
+    // BX_PLATFORM_EMSCRIPTEN
+
+    context_t context;
+    context.width = width;
+    context.height = height;
+    //context.program = program;
+    context.window = window;
+    //context.vbh = vbh;
+    //context.ibh = ibh;
+
+#if BX_PLATFORM_EMSCRIPTEN
+    emscripten_set_main_loop_arg( main_loop, &context, -1, 1 );
+#else
+    while ( !context.quit ) {
+        main_loop( &context );
+    }
+#endif // BX_PLATFORM_EMSCRIPTEN
+
+    //bgfx::destroy( vbh );
+    //bgfx::destroy( ibh );
+    //bgfx::destroy( program );
+
+    ImGui_ImplSDL2_Shutdown( );
+    ImGui_Implbgfx_Shutdown( );
+
+    ImGui::DestroyContext( );
+    bgfx::shutdown( );
+
+    SDL_DestroyWindow( window );
+    SDL_Quit( );
     return 0;
 }
